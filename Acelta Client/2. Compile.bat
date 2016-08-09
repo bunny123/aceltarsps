4e5bccbc66335e81d5fedbd82ffea5fe257ad653
Luis <zunigapk@gmail.com>
2016-08-08 22:18:08 -0400

    Removed Acelta Client

--- Acelta Client/2. Compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Compiler
-:build
-cls
-"C:\Program Files\Java\jdk1.8.0_40\bin\javac.exe" *.java
-pause
-goto :build
--- Acelta Client/sign/2. Compile.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-title Compiler
-:build
-cls
-"C:\Program Files\Java\jdk1.6.0_25\bin\javac.exe" *.java
-pause
-goto :build
